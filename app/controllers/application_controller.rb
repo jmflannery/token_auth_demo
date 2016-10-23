fc5bdf0f8082f1da83cee12b162906800558270a
Jack Flannery <jmflannery81@gmail.com>
2016-10-23 16:03:39 -0400

    Must use null_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def authenticate_token
     @user = authenticate_with_http_token do |key, options|
