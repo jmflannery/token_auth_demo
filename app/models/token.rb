0115739d9f2b40085e74febb50b63128bf881766
Jack Flannery <jmflannery81@gmail.com>
2016-10-23 11:26:37 -0400

    Add token model

---
+++ app/models/token.rb
@@ -0,0 +1,9 @@
+class Token < ApplicationRecord
+  belongs_to :user
+
+  def generate_key!
+    begin
+      self.key = Devise.friendly_token(64)
+    end while self.class.exists?(key: self.key)
+  end
+end
